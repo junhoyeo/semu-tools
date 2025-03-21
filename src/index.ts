b277483c6a4874bb2ba0d89d939f5ec2a88d4358
Junho Yeo <i@junho.io>
2025-03-21 20:43:02 +0900

    엑셀 파일 저장 방식을 변경

--- src/index.ts
+++ src/index.ts
@@ -2,58 +2,61 @@ import * as XLSX from 'xlsx';
 
 import { fetchISINCode } from './fetch-code';
 
-const STOCK_NAME_C_IDX = 0;
-const STOCK_CODE_C_IDX = 20;
+const STOCK_NAME_COL = 'A';
+const STOCK_CODE_COL = 'U';
 
 const main = async () => {
-  // read from xlsx file in same folder as package.json and header row 제외하고 각 줄의 첫 번째 cell을 읽음.
   const workbook = XLSX.readFile('2021 해외주식 양도소득세.xlsx');
   const sheetName = workbook.SheetNames[0];
-
   const sheet = workbook.Sheets[sheetName];
+  const data = XLSX.utils.sheet_to_json(sheet, { header: 'A' });
 
-  const range = XLSX.utils.decode_range(sheet['!ref']!);
+  const headers: any = data[0];
 
-  const headerCell =
-    sheet[XLSX.utils.encode_cell({ r: range.s.r, c: STOCK_NAME_C_IDX })];
-  if ((headerCell.v as string).trim() !== '주식 종목명') {
-    throw Error('첫 번째 셀의 값이 `주식 종목명`이 아닙니다.');
+  if (headers[STOCK_NAME_COL] !== '주식 종목명') {
+    throw Error(
+      `첫 번째 셀의 값이 \'주식 종목명\'이 아닙니다.': ${headers[STOCK_NAME_COL]}`,
+    );
   }
 
-  const ISIN_CODE_MAPPING = new Map<string, string>(); // query -> ISIN code
+  const ISIN_CODE_MAPPING = new Map<string, string>();
 
-  for (let R = range.s.r + 1; R <= range.e.r; R++) {
-    const cell = sheet[XLSX.utils.encode_cell({ r: R, c: STOCK_NAME_C_IDX })];
-    if (cell) {
-      const value = cell.v as string;
-      console.log(value);
+  for (let i = 1; i < data.length; i++) {
+    const row: any = data[i];
+    const stockName = row[STOCK_NAME_COL];
+    if (!stockName) {
+      throw Error(`주식 종목명이 없습니다.: ${i}`);
+    }
 
-      // query to ISIN code mapping
-      let isinCode = ISIN_CODE_MAPPING.get(value);
-      if (!isinCode) {
-        // fetch ISIN code from API
-        isinCode = (await fetchISINCode(value)) || '';
-        ISIN_CODE_MAPPING.set(value, isinCode);
+    // ISIN 코드 가져오기
+    let isinCode = ISIN_CODE_MAPPING.get(stockName);
+    if (!isinCode) {
+      isinCode = (await fetchISINCode(stockName)) || '';
+      console.log(`[FETCH] ${isinCode || 'Unknown'} for ${stockName}`);
+      if (isinCode) {
+        ISIN_CODE_MAPPING.set(stockName, isinCode);
       }
+    }
 
-      // STOCK_CODE_C_IDX에 ISIN code를 쓰기
-      const cellAddress =
-        sheet[XLSX.utils.encode_cell({ r: R, c: STOCK_CODE_C_IDX })];
-
-      // 셀에 ISIN 코드 쓰기
-      if (!sheet[cellAddress]) {
-        sheet[cellAddress] = { t: 's', v: isinCode };
-        console.log(`[CREATE] ${isinCode}`);
-      } else {
-        const currentValue = sheet[cellAddress].v;
-        if (currentValue !== isinCode) {
-          sheet[cellAddress].v = isinCode;
-        }
-      }
+    const currentValue = row[STOCK_CODE_COL];
+    if (!currentValue) {
+      row[STOCK_CODE_COL] = isinCode;
+      console.log(`[CREATE] ${isinCode}`);
+    } else if (currentValue !== isinCode) {
+      console.log(`[UPDATE] ${currentValue} -> ${isinCode}`);
+      row[STOCK_CODE_COL] = isinCode;
+    } else {
+      console.log(`[UNCHANGED] ${isinCode}`);
     }
   }
 
-  await XLSX.writeFileAsync('2021 해외주식 양도소득세_ISIN.xlsx', workbook, {});
+  const newSheet = XLSX.utils.json_to_sheet(data, { skipHeader: true });
+  const columnWidths = Array.from({ length: 21 }, () => ({ wch: 15 }));
+  newSheet['!cols'] = columnWidths;
+  newSheet['!ref'] = sheet['!ref'];
+  workbook.Sheets[sheetName] = newSheet;
+
+  XLSX.writeFile(workbook, '2021 해외주식 양도소득세_ISIN.xlsx');
   console.log('ISIN 코드 업데이트가 완료되었습니다.');
 };
 
